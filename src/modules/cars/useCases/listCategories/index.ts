386ffefd8e9cabefcbcb7f25505aa82790f2e18e
Fernando Otoni de Oliveira <feer.otoni@hotmail.com>
2023-03-08 18:22:44 -0300

    Delete src directory

--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { ListCategoriesController } from "./ListCategoriesController";
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
-
-const listCategoriesRepository = CategoriesRepository.getInstance()
-const listCategoriesUseCase = new ListCategoriesUseCase(listCategoriesRepository)
-const listCategoriesController = new ListCategoriesController(listCategoriesUseCase)
-
-export { listCategoriesController }
