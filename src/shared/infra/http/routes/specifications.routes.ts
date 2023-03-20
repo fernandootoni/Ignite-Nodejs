f5d9e2039df04a3d1b4538ca24888109b9d9239f
Fernando Otoni de Oliveira <feer.otoni@hotmail.com>
2023-03-20 20:03:09 -0300

    Add files via upload

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,17 @@
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController'
 import { Router } from 'express'
+import { ensureAdmin } from '../middlewares/ensureAdmin'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 
 const specificationRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
 
-specificationRoutes.use(ensureAuthenticated)
-specificationRoutes.post("/", createSpecificationController.handle)
+specificationRoutes.post(
+  "/", 
+  ensureAuthenticated, 
+  ensureAdmin, 
+  createSpecificationController.handle
+)
 
 export { specificationRoutes }
--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -3,7 +3,7 @@ import { hash } from 'bcryptjs'
 import createConnection from '../data-source'
 
 async function create() {
-  const connection = await createConnection()
+  const connection = await createConnection("localhost")
 
   const id = uuidV4()
   const password = await hash("admin", 8)
