ecba5ea63390babbe0c102694e3a875b2a1c4bac
Fernando Otoni de Oliveira <feer.otoni@hotmail.com>
2023-03-28 16:01:07 -0300

    Add files via upload

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -8,12 +8,6 @@ async function create() {
   const id = uuidV4()
   const password = await hash("admin", 8)
 
-  // await connection.query(
-  //  `INSERT INTO USERS(id, name, email, password, admin, created_at)
-  //   VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, ${new Date()})
-  //  `
-  // )
-
   await connection.query(
     `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
     values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
